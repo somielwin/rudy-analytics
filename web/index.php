ef8f2314824d4882558d09f9455e07e17d34f02f
Denmark Mago <denmark.mago@searchoptmedia.com>
2017-10-17 19:56:09 +0800

    Update

--- web/index.php
+++ web/index.php
@@ -25,8 +25,20 @@ header("Access-Control-Allow-Origin: *");
         top.location.href="http://goo.gl/JjoF6N";
     }
 </script>
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WDKT4X6');</script>
+	<!-- End Google Tag Manager -->
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDKT4X6"
+				  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <section id="main-container">
 
 	<div class="popup-wrap default-popup" id="mo-confirmation-submit" >
