fe30ef8db1cd8e95a8ab423a03aadccd11b6f2a1
Denmark Mago <denmark.mago@searchoptmedia.com>
2017-10-19 13:54:14 +0800

    Fix analytics trigger

--- web/js/custom.js
+++ web/js/custom.js
@@ -79,7 +79,7 @@ function bindEvents() {
     $dom.on('click', '#btn-data-layer-unique', function(e) {
         //console.log('analytics fired!');
         //dataLayer.push({'event' : 'formSubmitted', 'Register' : 'RudyProject'});
-        fbq('trackCustom', 'Registration1');
+        fbq('trackCustom', 'Registration1', {});
     });
 
     $dom.on('click', btnJoin, function(e) {
