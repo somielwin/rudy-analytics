ad14ab3e919ee40442fb02497cf3a8aa41aa9df3
Denmark Mago <denmark.mago@searchoptmedia.com>
2017-10-19 15:20:12 +0800

    Fix analytics trigger

--- web/index.php
+++ web/index.php
@@ -26,39 +26,20 @@ header("Access-Control-Allow-Origin: *");
     }
 </script>
 
-<!--	<!-- Google Tag Manager -->
-<!--	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':-->
-<!--			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],-->
-<!--			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=-->
-<!--			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);-->
-<!--		})(window,document,'script','dataLayer','GTM-WDKT4X6');</script>-->
-<!--	<!-- End Google Tag Manager -->
-
-	<script>
-		!function(f,b,e,v,n,t,s)
-		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-			n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-			if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-			n.queue=[];t=b.createElement(e);t.async=!0;
-			t.src=v;s=b.getElementsByTagName(e)[0];
-			s.parentNode.insertBefore(t,s)}(window, document,'script',
-			'https://connect.facebook.net/en_US/fbevents.js');
-		fbq('init', '1995678204034709');
-		fbq('track', 'PageView');
-	</script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WDKT4X6');</script>
+	<!-- Google Tag Manager -->
 </head>
 <body>
 <!--<!-- Google Tag Manager (noscript) -->
-<!--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDKT4X6"-->
-<!--				  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>-->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDKT4X6"
+				  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!--<!-- End Google Tag Manager (noscript) -->
 
-<!-- Facebook Pixel Code -->
-
-<noscript><img height="1" width="1" style="display:none"
-			   src="https://www.facebook.com/tr?id=1995678204034709&ev=PageView&noscript=1"
-		/></noscript>
-<!-- End Facebook Pixel Code -->
 
 <section id="main-container">
 	<a href="javascript:void(0)" id="btn-data-layer-unique" style="text-decoration: none;" onclick="">&nbsp;</a>
